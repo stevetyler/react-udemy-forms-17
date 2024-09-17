3f6068f3cb427347b6cfbf861d40434995101d88
Steve.Tyler <steve.tyler@centrica.com>
2024-09-17 17:57:09 +0100

    enabled signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from './components/Header.jsx';
-import Login from './components/StateLogin.jsx';
+import Login from './components/Login.jsx'; // switch as necessary
+import Signup from './components/Signup.jsx';
 
 function App() {
   return (
     <>
-      <Header />
       <main>
-        <Login />
+        {/* <Login /> */}
+        <Signup />
       </main>
     </>
   );
